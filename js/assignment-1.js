a737b8e7605d98257191526e224d800c02cdbb63
Sarah Perdomo <sarahdanielap@gmail.com>
2025-06-16 19:56:23 -0400

    first part of assignment 1

---
+++ js/assignment-1.js
@@ -0,0 +1,9 @@
+const firstColumn = ["The turkey","Mom", "Dad","The dog","My teacher", "The elephant","The cat"];
+const secondColumn = ["sat on", "ate", "danced with","saw","doesn't like","kissed"];
+const thirdColumn = ["a funny", "a scary", "a goofy", "a slimy","a barking","a fat"];
+const fourthColumn = ["goat", "monkey", "fish","cow","frog","bug","worm"];
+const fifthColumn = ["on the moon.", "on the chair.","in my spaghetti.","in my soup.","on the grass","in my shoes."];
+const buttons = document.querySelectorAll("button")
+const displayText = document.querySelector(".display-text");
+let space = ["","","","","",""];
+
