2601b018c48277bd0c6021e03a1c9832ede5af94
Sarah Perdomo <sarahdanielap@gmail.com>
2025-06-18 20:45:58 -0400

    added semicolon and alt to pass the validators

--- js/assignment-1.js
+++ js/assignment-1.js
@@ -1,20 +1,20 @@
 const firstColumn = ["The turkey","Mom", "Dad","The dog","My teacher", "The elephant","The cat"];
 const firstImage = ["turkey.jpg","mom.jpg","dad.jpg","dog.jpg","teacher.jpg","elephant.jpg","cat.jpg"];
 const secondColumn = ["sat on", "ate", "danced with","saw","doesn't like","kissed"];
-const secondImage = ["sat-on.jpg","ate.jpg","dance-with.jpg","saw.jpg","doesnt-like.jpg","kiss.jpg"]
+const secondImage = ["sat-on.jpg","ate.jpg","dance-with.jpg","saw.jpg","doesnt-like.jpg","kiss.jpg"];
 const thirdColumn = ["a funny", "a scary", "a goofy", "a slimy","a barking","a fat"];
 const thirdImage = ["funny.jpg","scary.jpg","goofy.jpg","slimy.jpg","barking.jpg","fat.jpg"];
 const fourthColumn = ["goat", "monkey", "fish","cow","frog","bug","worm"];
 const fourthImage = ["goat.jpg","monkey.jpg","fish.jpg","cow.jpg","frog.jpg","bug.jpg","worm.jpg"];
 const fifthColumn = ["on the moon.", "on the chair.","in my spaghetti.","in my soup.","on the grass","in my shoes."];
-const fifthImage = ["moon.jpg","chair.jpg","spaghetti.jpg","soup.jpg","grass.jpg","shoe.jpg"]
-const buttons = document.querySelectorAll("button")
+const fifthImage = ["moon.jpg","chair.jpg","spaghetti.jpg","soup.jpg","grass.jpg","shoe.jpg"];
+const buttons = document.querySelectorAll("button");
 const displayText = document.querySelector(".display-text");
 const index = [0,0,0,0,0];
 const columns = [firstColumn,secondColumn,thirdColumn,fourthColumn,fifthColumn];
 const AllImages = [firstImage,secondImage,thirdImage,fourthImage,fifthImage];
 const images = document.querySelectorAll("img");
-const surprise = document.querySelector(".Surprise")
+const surprise = document.querySelector(".Surprise");
 
 buttons.forEach((button, i) => {
     if (i < 5) {
@@ -25,16 +25,16 @@ buttons.forEach((button, i) => {
                 images[i].src = "images/" + AllImages[i][index[i]];
             }
             textDisplayed();
-        })
+        });
     }
-})
+});
 surprise.addEventListener("click", () => {
     for (let i = 0; i < columns.length; i++) {
         index[i] = Math.floor(Math.random() * columns[i].length);
         images[i].src = "images/" + AllImages[i][index[i]];
     }
     textDisplayed();
-})
+});
 function textDisplayed() {
  displayText.textContent = `${columns[0][index[0]]} ${columns[1][index[1]]} ${columns[2][index[2]]} ${columns[3][index[3]]} ${columns[4][index[4]]}`;
 }
